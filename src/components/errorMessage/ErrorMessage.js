f8022f53828c7ca85e458e00a29affcafb5badb9
Abylai <abylaikhhann@mail.ru>
2022-10-31 15:43:46 +0500

    create task marvel

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,10 @@
+
+import error from "./error.gif";
+
+const ErrorMessage = () => {
+    return (
+        <img style={{margin: '0 auto', background: 'none', display: 'block'}} width="200px" height="200px" src={error} alt="error"/>
+    )
+}
+
+export default ErrorMessage;
---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,9 @@
+import spinner from "./spinner.gif";
+
+const Spinner = () => {
+    return (
+        <img style={{margin: '0 auto', background: 'none', display: 'block'}} width="64px" height="64px" src={spinner} alt="spinner"/>
+    )
+}
+
+export default Spinner;
